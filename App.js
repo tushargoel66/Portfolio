879fe4d37563adbf6fe98a6797bb27341e56a906
TUSHAR GOEL <tushargoel66@gmail.com>
2020-06-07 12:51:32 +0530

    modification

--- App.js
+++ App.js
@@ -3,5 +3,11 @@
     angular.module('Portfolio',[])
     .controller('Name',function ($scope){
         $scope.name="TUSHAR GOEL";
+    })
+    .controller('Header',function($scope){
+        $scope.head="About me.";
+    })
+    .controller('Aboutme',function($scope){
+        $scope.aboutme="Hey everyone I'm TUSHAR GOEL working at Virtusa as an Intern. I collab with people to get the things done on various technologies like JAVA, GO and many more. At present I am pursuing my Bachlore's degree from CMR Engineering Collge, Hyderabad. The technologies I'm intrested in and have a knowlwdge are Bootstrap, JAVA, GO, PHP, Servlets, DBMS and many more.";
     });
 })();
